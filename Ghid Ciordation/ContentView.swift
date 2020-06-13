2a0ce5ab65b0294592e4a5f92f0c60094f3594c2
Mugurel Moscaliuc <mugur.moscaliuc@gmail.com>
2020-06-13 22:04:58 +0100

    Initial Commit

---
+++ Ghid Ciordation/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Ghid Ciordation
+//
+//  Created by Mugurel Moscaliuc on 13/06/2020.
+//  Copyright © 2020 Mugurel Moscaliuc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
